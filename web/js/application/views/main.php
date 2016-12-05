db22c956c0f7dc435399f1e595cf1f92bc709057
xzessmedia <tim@xzessmedia.de>
2016-12-05 18:58:30 +0100

    Translation: Startpage

--- web/js/application/views/main.php
+++ web/js/application/views/main.php
@@ -4,7 +4,7 @@
 			<img src="images/logo72.png"><br/>
 		</div>
 		</br>
-		<h1>Übersicht</h1>
+		<h1>{{ 'general.Overview' | i18n }}</h1>
 		</br>
 		</br>
 		
@@ -12,14 +12,14 @@
 
 		    <div class="row">
 			<div class="col-sm-4">
-				<h3>Versionsübersicht:</h3>
-				<p><strong>Aktuelle Version:</strong>  {{version}}</p>
+				<h3>{{ 'general.VersionOverview' | i18n }}:</h3>
+				<p><strong>{{ 'general.CurrentVersion' | i18n }}:</strong>  {{version}}</p>
 				<hr>
 				<div class="embed-responsive embed-responsive-16by9">
 				<iframe width="460" height="215" src="https://www.youtube.com/embed/Wkv_HUbRlu4" frameborder="0" allowfullscreen></iframe>
 				</div>
 				<hr>
-				<h4>Letzte Änderungen:</h4>
+				<h4>{{ 'general.LastChanges' | i18n }}:</h4>
 				 <div class="panel-group" ng-repeat="item in updates">
 				    <div class="panel panel-default">
 				      <div class="panel-heading"><strong>{{item.Title}}</strong></div>
@@ -29,7 +29,7 @@
 			</div>
 			
 			 <div class="col-sm-4">
-			<h3>Todo Liste:</h3>
+			<h3>{{ 'general.TodoList' | i18n }}:</h3>
 			 <hr>
 			   <div class="panel-group" ng-repeat="item in todo">
 				    <div class="panel panel-default">
@@ -38,7 +38,7 @@
 			    </div>
 			</div>  
 			</br>
-			<h3>Bug Liste:</h3>
+			<h3>{{ 'general.BugList' | i18n }}:</h3>
 			<hr>
 				   <div class="panel-group" ng-repeat="item in bugs">
 			    <div class="panel panel-default">
@@ -48,7 +48,7 @@
 				  </div>
 			</div>
 			<div class="col-sm-4">
-				<h3>Buschfunk</h3>
+				<h3>{{ 'general.Discussion' | i18n }}</h3>
 			<div class="fb-comments" data-href="https://www.facebook.com/bewerbungsapp/" data-width="300" data-numposts="5"></div>
 	  		</div> 
 		
