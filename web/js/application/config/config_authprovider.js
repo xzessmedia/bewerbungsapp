40a1d54cf42645f5a588f27b936fa01d64de7a49
xzessmedia <tim@xzessmedia.de>
2016-12-05 14:37:19 +0100

    Satellizer default value override

--- web/js/application/config/config_authprovider.js
+++ web/js/application/config/config_authprovider.js
@@ -4,6 +4,11 @@
  * @Last Modified by:   Tim Koepsel 
  * @Last Modified time: 2016-11-29 15:52:52 
  */
+
+/*************************************************************************
+* This Configuration is used by satellizer module
+* ***********************************************************************
+*/
 'use strict';
 
 var app = angular.module('ApplicationApp');
@@ -12,6 +17,20 @@ $authProvider.facebook({
       clientId: '525143354344103',
       responseType: 'token'
     });
+
+// Overriding default values
+$authProvider.httpInterceptor = function() { return true; },
+$authProvider.withCredentials = false;
+$authProvider.tokenRoot = null;
+$authProvider.baseUrl = '/';
+$authProvider.loginUrl = '/auth/login';
+$authProvider.signupUrl = '/auth/signup';
+$authProvider.unlinkUrl = '/auth/unlink/';
+$authProvider.tokenName = 'token';
+$authProvider.tokenPrefix = 'satellizer';
+$authProvider.tokenHeader = 'Authorization';
+$authProvider.tokenType = 'Bearer';
+$authProvider.storageType = 'localStorage';
 /*
     $authProvider.google({
       clientId: 'Google Client ID'
